909f7065413233920a0bd6b6b326c5f885088177
DrozzzD35 <kapustin3595@yandex.ru>
2025-08-17 10:20:56 +0300

    Проведён урок 16.08.2025 В классе main созданы методы для удобства, чистоты и большего порядка. Добавлен файл для 3го этапа

--- src/MyTest.java
+++ src/MyTest.java
@@ -1,55 +1,84 @@
-import model.TaskStatus;
 import model.Task;
-import service.*;
-
-import java.util.LinkedList;
+import model.TaskStatus;
+import service.HistoryManager;
+import service.Managers;
+import service.TaskManager;
 
 public class MyTest {
 
+    private static HistoryManager<Task> history;
+    private static TaskManager<Task> manager;
+
     public static void main(String[] args) {
-        HistoryManager<Task> history = Managers.getDefaultHistory();
-        TaskManager<Task> manager = Managers.getDefault(history);
+        history = Managers.getDefaultHistory();
+        manager = Managers.getDefault(history);
 
 
         // Создание задачи
-        Task task1 = new Task("task1", "task1");
-        manager.add(task1);
-        Task existTask1 = manager.getTaskById(task1.getId());
-        System.out.println(existTask1);
+        Task task1 = createTask();
+        Task task2 = createTask();
+        Task task3 = createTask();
 
-        // Обновление задачи
-        Task updateTask1 = new Task("updateTask1", "updateTask1");
-        updateTask1.setStatus(TaskStatus.IN_PROGRESS);
+        updateTaskStatus(task1.getId());
 
-        manager.updateTask(updateTask1, task1.getId());
-
-
-        System.out.println(manager.getTaskById(task1.getId()));
+        System.out.println("===========  Таски в памяти   ===============");
         System.out.println(manager.getTasks());
-        System.out.println();
+
+        System.out.println("==========================\n\n");
+
+        addTaskHistory(task1);
+
+
+        System.out.println("===========  История   ===============");
+        System.out.println("История " + history.getHistory());
+        System.out.println("=========================\n\n");
+
+//        manager.updateTask(updateTask1, task1.getId());
+//
+//
+//        System.out.println(manager.getTaskById(task1.getId()));
+//        System.out.println(manager.getTasks());
+//        System.out.println();
+
 
         //Тест истории
-        Task task2 = new Task("task2", "task2");
-        manager.add(task2);
-        manager.add(new Task("t3", "t3"));
-        manager.add(new Task("t4", "t4"));
-        manager.getTaskById(task1.getId());
-        manager.getTaskById(task1.getId());
-        manager.getTaskById(task1.getId());
-        manager.getTaskById(task1.getId());
-        manager.getTaskById(task1.getId());
-        manager.getTaskById(task1.getId());
-        manager.getTaskById(task1.getId());
-        manager.getTaskById(task2.getId());
-        manager.getTaskById(task1.getId());
-        manager.getTaskById(task2.getId());
+//        Task task2 = new Task("task2", "task2");
+//        manager.add(task2);
+//        manager.add(new Task("t3", "t3"));
+//        manager.add(new Task("t4", "t4"));
+//        manager.getTaskById(task1.getId());
+//        manager.getTaskById(task1.getId());
+//        manager.getTaskById(task1.getId());
+//        manager.getTaskById(task1.getId());
+//        manager.getTaskById(task1.getId());
+//        manager.getTaskById(task1.getId());
+//        manager.getTaskById(task1.getId());
+//        manager.getTaskById(task2.getId());
+//        manager.getTaskById(task1.getId());
+//        manager.getTaskById(task2.getId());
 //        System.out.println("Все задачи: ");
 //        System.out.println(manager.getTasks());
 
-        System.out.println();
-        System.out.println("История " + history.getHistory());
+//        System.out.println();
+//        System.out.println("История " + history.getHistory());
 
 
+    }
+
+    private static void addTaskHistory(Task task) {
+        manager.getTaskById(task.getId());
+    }
+
+    private static void updateTaskStatus(int currentTaskId) {
+        Task updateTask = new Task("updateTask", "updateTask");
+        updateTask.setStatus(TaskStatus.IN_PROGRESS);
+        manager.updateTask(updateTask, currentTaskId);
+    }
+
+    private static Task createTask() {
+        Task task = new Task("task", "task");
+        manager.add(task);
+        return task;
     }
 
 }
