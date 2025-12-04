ed22af19b281bc670d02b58a8a8cb9456a380756
DrozzzD35 <kapustin3595@yandex.ru>
2025-12-04 17:17:33 +0300

    feat(HttpClient.java): добавить методы, добавить внешние переменные

--- src/client/HttpClient.java
+++ src/client/HttpClient.java
@@ -14,30 +14,19 @@ public class HttpClient<T extends Task> {
     private final java.net.http.HttpClient client;
     private final String url = "http://localhost:8080/";
     private final String urlTask = "tasks/task";
+    private final String urlEpic = "tasks/epic";
+    private final String urlSubTask = "tasks/subtask";
+    private final String urlHistory = "tasks/history";
+    private final String urlTasks = "tasks";
     private final String urlId = "?id=";
+    Map<String, String> task = new HashMap<>();
     private final Gson gson = new Gson();
 
     public HttpClient(java.net.http.HttpClient client) {
         this.client = client;
     }
 
-    public HttpResponse<String> createTaskClient(String name, String description) throws IOException, InterruptedException {
-        Map<String, String> task = new HashMap<>();
-        task.put("name", name);
-        task.put("description", description);
-        String json = gson.toJson(task);
-
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(url + "tasks/task"))
-                .POST(HttpRequest.BodyPublishers.ofString(json))
-                .header("Content-Type"
-                        , "application/json; Charset=UTF-8")
-                .build();
-
-        return client.send(request, HttpResponse.BodyHandlers.ofString());
-    }
-
-    public HttpResponse<String> getTaskByIdClient(int id) throws IOException, InterruptedException {
+    public HttpResponse<String> getTaskById(int id) throws IOException, InterruptedException {
         HttpRequest request = HttpRequest.newBuilder()
                 .GET()
                 .uri(URI.create(url + urlTask + urlId + id))
@@ -48,16 +37,150 @@ public class HttpClient<T extends Task> {
         return client.send(request, HttpResponse.BodyHandlers.ofString());
     }
 
-    public HttpResponse<String> getTasksClient() throws IOException, InterruptedException {
+    public HttpResponse<String> getTasks() throws IOException, InterruptedException {
         HttpRequest request = HttpRequest.newBuilder()
                 .GET()
                 .uri(URI.create(url + urlTask))
                 .header("Content-Type"
-                ,"application/json; Charset=UTF-8")
+                        , "application/json; Charset=UTF-8")
                 .build();
 
         return client.send(request, HttpResponse.BodyHandlers.ofString());
     }
+
+    public HttpResponse<String> updateTask(int id, String name, String description) throws IOException, InterruptedException {
+        Map<String, String> task = new HashMap<>();
+        String json = getJson(task, name, description);
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .PUT(HttpRequest.BodyPublishers.ofString(json))
+                .uri(URI.create(url + urlTask + urlId + id))
+                .header("Content-Type"
+                        , "application/json; Charset=UTF-8")
+                .build();
+
+        return client.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+
+    public HttpResponse<String> createTask(String name, String description) throws IOException, InterruptedException {
+        Map<String, String> task = new HashMap<>();
+        String json = getJson(task, name, description);
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url + urlTask))
+                .POST(HttpRequest.BodyPublishers.ofString(json))
+                .header("Content-Type"
+                        , "application/json; Charset=UTF-8")
+                .build();
+
+        return client.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+
+    public HttpResponse<String> removeTask(int id) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url + urlTask + urlId + id))
+                .DELETE()
+                .header("Content-Type"
+                        , "application/json; Charset=UTF-8")
+                .build();
+
+        return client.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+
+    public HttpResponse<String> removeTasks() throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url + urlTask + urlId))
+                .DELETE()
+                .header("Content-Type"
+                        , "application/json; Charset=UTF-8")
+                .build();
+
+        return client.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+
+
+
+
+
+    public HttpResponse<String> getEpicById(int id) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create(url + urlEpic + urlId + id))
+                .header("Content-Type"
+                        , "application/json; Charset=UTF-8")
+                .build();
+
+        return client.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+
+    public HttpResponse<String> getEpics() throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create(url + urlEpic))
+                .header("Content-Type"
+                        , "application/json; Charset=UTF-8")
+                .build();
+
+        return client.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+
+    public HttpResponse<String> updateEpic(int id, String name, String description) throws IOException, InterruptedException {
+        String json = getJson(task, name, description);
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .PUT(HttpRequest.BodyPublishers.ofString(json))
+                .uri(URI.create(url + urlTask + urlId + id))
+                .header("Content-Type"
+                        , "application/json; Charset=UTF-8")
+                .build();
+
+        return client.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+
+    public HttpResponse<String> createEpic(String name, String description) throws IOException, InterruptedException {
+        String json = getJson(task, name, description);
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url + urlTask))
+                .POST(HttpRequest.BodyPublishers.ofString(json))
+                .header("Content-Type"
+                        , "application/json; Charset=UTF-8")
+                .build();
+
+        return client.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+
+    private String getJson(Map<String, String> task, String name, String description) {
+        task.put("name", name);
+        task.put("description", description);
+        String json = gson.toJson(task);
+        return json;
+    }
+
+    public HttpResponse<String> removeEpic(int id) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url + urlTask + urlId + id))
+                .DELETE()
+                .header("Content-Type"
+                        , "application/json; Charset=UTF-8")
+                .build();
+
+        return client.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+
+    public HttpResponse<String> removeEpics() throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url + urlTask + urlId))
+                .DELETE()
+                .header("Content-Type"
+                        , "application/json; Charset=UTF-8")
+                .build();
+
+        return client.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+
+
+
 
 
 
