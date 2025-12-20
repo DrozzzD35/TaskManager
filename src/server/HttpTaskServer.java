ab32f8d9db4d299f34d69735a1b08a056f7e4a26
DrozzzD35 <kapustin3595@yandex.ru>
2025-12-20 16:03:29 +0300

    feat: внедрить Config.java

--- src/server/HttpTaskServer.java
+++ src/server/HttpTaskServer.java
@@ -14,14 +14,14 @@ public class HttpTaskServer {
     private final HttpServer server;
 
     public HttpTaskServer(TaskManager<Task> taskManager) throws IOException {
-        int port = 8080;
-        this.server = HttpServer.create(new InetSocketAddress(port), 0);
+        Config config = new Config();
+        this.server = HttpServer.create(new InetSocketAddress(config.getPort()), 0);
 
-        server.createContext("/tasks/task", new TaskHandler<>(taskManager));
-        server.createContext("/tasks/epic", new EpicHandler<>(taskManager));
-        server.createContext("/tasks/subTask", new SubTaskHandler<>(taskManager));
-        server.createContext("/tasks/history", new HistoryHandler<>(taskManager));
-        server.createContext("/tasks", new TasksHandler<>(taskManager));
+        server.createContext(config.getTasks() + config.getTask(), new TaskHandler<>(taskManager));
+        server.createContext(config.getTasks() + config.getEpic(), new EpicHandler<>(taskManager));
+        server.createContext(config.getTasks() + config.getSubTask(), new SubTaskHandler<>(taskManager));
+        server.createContext(config.getTasks() + config.getHistory(), new HistoryHandler<>(taskManager));
+        server.createContext(config.getTasks(), new TasksHandler<>(taskManager));
 
     }
 
