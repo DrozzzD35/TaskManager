537b694a82241b65227a2104d20f70144f2345c3
DrozzzD35 <kapustin3595@yandex.ru>
2025-12-20 16:04:03 +0300

    refactor: поправить отступы строк

--- src/service/Managers.java
+++ src/service/Managers.java
@@ -15,21 +15,21 @@ public final class Managers {
     }
 
     public static <T extends Task> TaskManager<T> getDefault() {
-        if(inMemoryTaskManager == null){
+        if (inMemoryTaskManager == null) {
             inMemoryTaskManager = new InMemoryTaskManager<T>();
         }
         return inMemoryTaskManager;
     }
 
     public static <T extends Task> TaskManager<T> getDefaultFile(Path filePath) {
-        if(fileBackedTasksManager == null){
+        if (fileBackedTasksManager == null) {
             fileBackedTasksManager = new FileBackedTasksManager<T>(filePath);
         }
         return fileBackedTasksManager;
     }
 
     public static <T extends Task> HistoryManager<T> getDefaultHistory() {
-        if(inMemoryHistoryManager == null){
+        if (inMemoryHistoryManager == null) {
             inMemoryHistoryManager = new InMemoryHistoryManager<T>();
         }
         return inMemoryHistoryManager;
