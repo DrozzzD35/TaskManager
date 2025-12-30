9f0c84fb59e345a4b6e4704c3683be0cfad79e3d
DrozzzD35 <kapustin3595@yandex.ru>
2025-12-30 18:39:23 +0300

    feat: написать HttpTaskManager.java

--- src/service/Managers.java
+++ src/service/Managers.java
@@ -1,6 +1,7 @@
 package service;
 
 import model.Task;
+import server.HttpTaskManager;
 
 import java.nio.file.Path;
 
@@ -9,17 +10,18 @@ public final class Managers {
     private static TaskManager inMemoryTaskManager;
     private static TaskManager fileBackedTasksManager;
     private static HistoryManager inMemoryHistoryManager;
+    private static HttpTaskManager httpTaskManager;
 
     private Managers() {
         throw new UnsupportedOperationException("Утилитарный класс");
     }
 
-    public static <T extends Task> TaskManager<T> getDefault() {
-        if (inMemoryTaskManager == null) {
-            inMemoryTaskManager = new InMemoryTaskManager<T>();
-        }
-        return inMemoryTaskManager;
-    }
+//    public static <T extends Task> TaskManager<T> getDefault() {
+//        if (httpTaskManager == null) {
+//            httpTaskManager = new HttpTaskManager();
+//        }
+//        return httpTaskManager;
+//    }
 
     public static <T extends Task> TaskManager<T> getDefaultFile(Path filePath) {
         if (fileBackedTasksManager == null) {
