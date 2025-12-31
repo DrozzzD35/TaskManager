5ca3f15a4e3cb9f57f991b1e7827d0c140b8c41d
DrozzzD35 <kapustin3595@yandex.ru>
2025-12-31 06:33:02 +0300

    feat: изменить конструктор, теперь он работает с сервером, а не с файлом

--- src/server/Application.java
+++ src/server/Application.java
@@ -1,8 +1,6 @@
 package server;
 
-import model.Task;
-import service.Managers;
-import service.TaskManager;
+import service.HttpTaskServer;
 
 import java.io.IOException;
 import java.nio.file.Path;
@@ -10,9 +8,9 @@ import java.nio.file.Path;
 public class Application {
 
     public static void main(String[] args) throws IOException {
-        Path path = Path.of("src/dataBacked/FileBacked.CSV");
-        TaskManager<Task> fileBackedTasksManager = Managers.getDefaultFile(path);
-        HttpTaskServer httpTaskServer = new HttpTaskServer(fileBackedTasksManager);
+//        Path path = Path.of("src/dataBacked/FileBacked.CSV");
+//        TaskManager<Task> fileBackedTasksManager = Managers.getDefaultFile(path);
+        HttpTaskServer httpTaskServer = new HttpTaskServer();
         httpTaskServer.start();
     }
 }
