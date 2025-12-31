81582019ab2931ffe6763d7d8d64f301b3953ac8
DrozzzD35 <kapustin3595@yandex.ru>
2025-12-31 21:13:06 +0300

    refactor: удалить лишний код

--- src/server/Application.java
+++ src/server/Application.java
@@ -3,13 +3,10 @@ package server;
 import service.HttpTaskServer;
 
 import java.io.IOException;
-import java.nio.file.Path;
 
 public class Application {
 
     public static void main(String[] args) throws IOException {
-//        Path path = Path.of("src/dataBacked/FileBacked.CSV");
-//        TaskManager<Task> fileBackedTasksManager = Managers.getDefaultFile(path);
         HttpTaskServer httpTaskServer = new HttpTaskServer();
         httpTaskServer.start();
     }
