61d62736273561b1ddf63f12ba8c93243c2525fc
DrozzzD35 <kapustin3595@yandex.ru>
2026-01-02 19:51:39 +0300

    feat: добавить метод addTask

--- src/tests/HttpServerTest.java
+++ src/tests/HttpServerTest.java
@@ -4,11 +4,13 @@ import com.google.gson.Gson;
 import model.Task;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import server.KVServer.KVServer;
 import service.HttpTaskServer;
 import service.Managers;
 import service.TaskManager;
 import utils.GsonFactory;
+import utils.TestUtils;
 
 import java.io.IOException;
 
@@ -29,10 +31,15 @@ public class HttpServerTest {
     }
 
     @AfterEach
-    void stopServers(){
+    void stopServers() {
         kvServer.stop();
         taskServer.stop();
+    }
 
+    @Test
+    void addTask() {
+        TestUtils.createTask
+                (taskManager, "task1", "task1", "01.10.2020 00:00", 40);
     }
 
 
