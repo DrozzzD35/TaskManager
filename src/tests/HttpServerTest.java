b14843cede1eb183fc520c2683fe8ef3dcbaff0f
DrozzzD35 <kapustin3595@yandex.ru>
2026-01-04 04:15:04 +0300

    test: провести тест клиента с сервером

--- src/tests/HttpServerTest.java
+++ src/tests/HttpServerTest.java
@@ -1,8 +1,11 @@
 package tests;
 
+import client.HttpTaskClient;
 import com.google.gson.Gson;
 import model.Task;
+import model.Type;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import server.KVServer.KVServer;
@@ -13,12 +16,19 @@ import utils.GsonFactory;
 import utils.TestUtils;
 
 import java.io.IOException;
+import java.net.http.HttpClient;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.List;
 
-public class HttpServerTest {
+public class HttpServerTest<T extends Task> {
     private final Gson gson = GsonFactory.createGson();
     private TaskManager<Task> taskManager;
     private HttpTaskServer taskServer;
     private KVServer kvServer;
+    private HttpClient client;
+    private HttpTaskClient<Task> myClient;
 
     @BeforeEach
     void SetUp() throws IOException {
@@ -28,6 +38,9 @@ public class HttpServerTest {
         taskManager = Managers.getDefault();
         taskServer = new HttpTaskServer(taskManager);
         taskServer.start();
+
+        this.client = HttpClient.newHttpClient();
+        this.myClient = new HttpTaskClient<>(client);
     }
 
     @AfterEach
@@ -37,10 +50,22 @@ public class HttpServerTest {
     }
 
     @Test
-    void addTask() {
-        TestUtils.createTask
-                (taskManager, "task1", "task1", "01.10.2020 00:00", 40);
+    void addTask() throws IOException, InterruptedException {
+        HttpResponse<String> response = myClient.createTask(Type.TASK, "task1", "task1", LocalDateTime.parse("01.10.2020 00:00", GsonFactory.DATE_TIME_FORMATTER), Duration.ofMinutes(15));
+        Assertions.assertEquals(201, response.statusCode());
+
+        List<T> tasks = (List<T>) taskManager.getAllTasks();
+        Assertions.assertEquals(1, tasks.size(), "Список задач должен содержать 1 элемент");
     }
+
+//    @Test
+//    void getTask() throws IOException, InterruptedException {
+//        TestUtils.createTask(taskManager, "task1", "task1"
+//                , "01.11.2020 00:00", 15);
+//
+//        HttpResponse<String> response = myClient.getTaskById(1, Type.TASK);
+//        Assertions.assertEquals(200, response.statusCode());
+//    }
 
 
 }
